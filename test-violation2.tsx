4c29818009844a32b987434f20177c583a003e34
Sefatullah Omar <sefaullahomar1@gmail.com>
2025-10-01 15:18:45 -0700

    test: try to commit violations again

---
+++ test-violation2.tsx
@@ -0,0 +1,6 @@
+// This file intentionally violates best practices
+const bad_variable_name = "test"
+const UserComponent = (props: any) => {
+  console.log("debug message")
+  return <img src="test.jpg" />
+}
